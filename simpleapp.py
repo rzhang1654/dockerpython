3b015336b94106f91397209f367e14850301dab6
rzhang1654 <run.zhang@exeloncorp.com>
2020-07-22 14:21:51 -0400

    Add comment to simpleapp.py bu run zhang

--- simpleapp.py
+++ simpleapp.py
@@ -14,6 +14,7 @@ def hello_world():
     return "Hello world from Distelli & Docker!"
 
 if __name__ == '__main__':
+    'comment added'
     parser = optparse.OptionParser(usage="python simpleapp.py -p ")
     parser.add_option('-p', '--port', action='store', dest='port', help='The port to listen on.')
     (args, _) = parser.parse_args()
